09bc4c46668bb29447bbfa04c30aa6f811cb16b8
tham davies <thamdv96@gmail.com>
2018-01-03 10:01:05 +0700

    updated project structure

--- OneSystemManagement/Startup.cs
+++ OneSystemManagement/Startup.cs
@@ -101,6 +101,11 @@ namespace OneSystemManagement
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
