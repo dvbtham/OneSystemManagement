5c62c396aad85a5edcc13d099a9ef463a6a88b33
tham davies <thamdv96@gmail.com>
2018-03-30 09:14:16 +0700

    xem mật khẩu.

--- OneSystemManagement/Controllers/Resources/UserGridResource.cs
+++ OneSystemManagement/Controllers/Resources/UserGridResource.cs
@@ -11,6 +11,7 @@ namespace OneSystemManagement.Controllers.Resources
         public int Id { get; set; }
 
         public string Email { get; set; }
+        public string Password { get; set; }
 
         public string Phone { get; set; }
         public string FullName { get; set; }
