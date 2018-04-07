7e501e68be6a3955a634512f106305ad0685fb53
tham davies <thamdv96@gmail.com>
2018-04-07 10:41:06 +0700

    thêm file bower.

--- OneSystemManagement/Controllers/Resources/UserGridResource.cs
+++ OneSystemManagement/Controllers/Resources/UserGridResource.cs
@@ -11,7 +11,6 @@ namespace OneSystemManagement.Controllers.Resources
         public int Id { get; set; }
 
         public string Email { get; set; }
-        public string Password { get; set; }
 
         public string Phone { get; set; }
         public string FullName { get; set; }
